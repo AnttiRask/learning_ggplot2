e326d0ee8317504fdb6eab3d993692c67eeb3b8d
Antti Rask <anttilennartrask@gmail.com>
2026-04-06 01:36:24 +0300

    Renv update

--- renv/activate.R
+++ renv/activate.R
@@ -3,7 +3,7 @@ local({
 
   # the requested version of renv
   version <- "1.2.0"
-  attr(version, "md5") <- NULL
+  attr(version, "md5") <- "b7d230b07507f361d3bcf794d157a188"
   attr(version, "sha") <- NULL
 
   # the project directory
